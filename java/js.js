f9aca12aef7a756d31dcc190b52317528f3fb2d8
matinjamal <162322083+matinjamal@users.noreply.github.com>
2024-03-09 17:02:36 +0430

    Delete java directory

--- java/js.js
+++
@@ -1,6 +0,0 @@
-const btnNavEl = document.querySelector(".header-btn");
-const headerNavEl = document.querySelector(".header");
-
-btnNavEl.addEventListener("click", function () {
-  headerNavEl.classList.toggle("nav-open");
-});
